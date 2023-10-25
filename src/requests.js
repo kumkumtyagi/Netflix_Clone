a57ca9e53e4e1b30f33956fb34c6e822e6b43500
Kumkum Tyagi <tyagikumkum1200121@gmail.com>
2023-10-25 21:46:02 +0530

    first commit

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY="53a64de59062790b0256d531ed82d4d5";
+
+const requests={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&width_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
